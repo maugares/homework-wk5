669041c6ca90e1021929bcd816ef0c8dde48af2f
maugares <mauro.garciaesteban@gmail.com>
2019-04-20 22:21:23 +0200

    revert name folder CreateEventto EventForm

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connectionString = process.env.PORT || 'postgres://postgres:12341234@localhost:5432/postgres'
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:12341234@localhost:5432/postgres'
 const sequelize = new Sequelize(connectionString, { define: { timestamps: false, logging: false} })
 
 sequelize.sync({ force: false })
